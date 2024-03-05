d024a8810a929eff8fec5ff1c3d668397975a0dc
MicheleDados <162336225+MicheleDados@users.noreply.github.com>
2024-03-05 12:02:23 -0300

    Add files via upload

---
+++ api_financ.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+from flask import Flask
+from flask import jsonify
+
+st.set_page_config(page_title="API - Movimento")
+
+with st.container():
+    st.subheader("API com movimento financeiro da empresa")
+    
+
+tabela = pd.read_excel("Base Financeiro.xlsx")
+    
+
+app = Flask(__name__) # Cria o site
+
+@app.route('/')
+def movimento():
+  dic_movimento = tabela.to_dict()
+  return jsonify(dic_movimento)
+
+@app.route('/movimento/banco/<banco>') # Decorater -> Diz em qual link a função vai rodar
+def movimento_banco(banco):
+  tabela_banco = tabela[['Banco', 'Valor da Movimentação']].groupby('Banco').sum()
+  if banco in tabela_banco.index:
+    valor_banco = tabela_banco.loc[banco]
+    dic_banco = valor_banco.to_dict()
+  return jsonify({banco: dic_banco})
+
+
+
+if __name__ == '__main__':
+    app.run()
